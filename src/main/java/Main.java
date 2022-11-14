17d3854baa2ac8c41da41367fe1b9e2e0e283e62
Jonathán <solyomjonathan@gmail.com>
2022-11-14 20:43:06 +0200

    Curs 16

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,11 +2,11 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-    List<Person> personList = List.of(new Person("Maria","Anna",20,"Baia Mare"),
-            new Person("Anca","Petru",15,"Satu Mare"),
-            new Person("John","Pete",70,"NY"),
-        new Person("Anca","Petrescu",15,"Satu Mare"));
-    PersonService personService= new PersonService(personList);
+        List<Person> personList = List.of(new Person("Maria", "Anna", 20, "Baia Mare"),
+                new Person("Anca", "Petru", 15, "Satu Mare"),
+                new Person("John", "Pete", 70, "NY"),
+                new Person("Anca", "Petru", 30, "Oradea"));
+        PersonService personService = new PersonService(personList);
         System.out.println(personService.allThePersonsNames());
         System.out.println(personService.allPersonsThatAreMajor());
         System.out.println(personService.allFirstNameCapitalized());
@@ -15,6 +15,11 @@ public class Main {
         System.out.println(personService.personsHavingFirstNameStartingWithA());
         System.out.println(personService.firstNameUnique());
         System.out.println(personService.sortByFirstName());
+        System.out.println(personService.personsHavingFirstNameStartingWithA());
+        System.out.println(personService.multypleSort());
+        System.out.println(personService.allPersonsFromOradeaOrCluj());
+        System.out.println(personService.allPersonsFromOradea());
+        System.out.println(personService.sortByLastName());
     }
 
 }
